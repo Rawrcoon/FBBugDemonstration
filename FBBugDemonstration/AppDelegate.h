8c27ac3138077f3969f1cddd83285c5e25db9e88
Mark Oelsner <moelsner@mobivention.com>
2015-07-21 12:24:37 +0200

    Project init

---
+++ FBBugDemonstration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FBBugDemonstration
+//
+//  Created by Mark Oelsner on 21/07/15.
+//  Copyright (c) 2015 Mark Oelsner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FBBugDemonstration/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FBBugDemonstration
+//
+//  Created by Mark Oelsner on 21/07/15.
+//  Copyright (c) 2015 Mark Oelsner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
