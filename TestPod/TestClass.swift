2c717cdafbd4a2d941d3993101b8f8f813d5d10c
Mark Oelsner <moelsner@mobivention.com>
2015-07-21 16:14:04 +0200

    bug setup

---
+++ TestPod/TestClass.swift
@@ -0,0 +1,19 @@
+//
+//  TestClass.swift
+//  TestPod
+//
+//  Created by Sören Busch on 13.07.15.
+//  Copyright (c) 2015 test. All rights reserved.
+//
+
+import Foundation
+import FBSDKCoreKit
+
+public class TestClass: NSObject {
+	
+	public static func doStuff()
+	{
+		FBSDKUtility.dictionaryWithQueryString("lol")
+	}
+	
+}
